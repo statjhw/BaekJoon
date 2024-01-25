723a0fc748770e219cafa68acf6491fee2e8cd6d
j.hwp <we1905@icloud.com>
2024-01-25 16:35:46 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int change_int(char s) {
+    int base_s = 2;
+    if (s == 'A' || s == 'B' || 'C')
+        return (base_s += 1) * 2;
+    if ()
+}
+
