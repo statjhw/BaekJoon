e0eaf49baacb0ff98c26835e46e0227f64914190
j.hwp <we1905@icloud.com>
2024-01-25 18:08:18 +0900

    2444

--- main.cpp
+++ main.cpp
@@ -2,11 +2,19 @@
 #include <string>
 using namespace std;
 
+int find(int g, int b) {
+    if (g == b)
+        return 0;
+    else
+        return g - b;
+}
+
 int main() {
-    cout << "         ,r'\"7" << endl;
-    cout << "r`-_   ,'  ,/" << endl;
-    cout << " \\. \". L_r'" << endl;
-    cout << "   `~\\/" << endl;
-    cout << "      |" << endl;
-    cout << "      |" << endl;
+    int good[6] ={1,1,2,2,2,8};
+    int c;
+    for (int i=0; i < 6; i++) {
+        cin >> c;
+        cout << find(good[i], c) << " ";
+    }
+
 }
