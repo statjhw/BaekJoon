4ea4fed8ecc76d9bdc5b99a3e8bb98fcec47eef6
j.hwp <we1905@icloud.com>
2024-01-25 17:53:48 +0900

    11718*

--- main.cpp
+++ main.cpp
@@ -3,14 +3,8 @@
 using namespace std;
 
 int main() {
-    int time[26] = {3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,8,9,9,9,
-                    10,10,10,10};
-    string word;
-    cin >> word;
-    int s = 0;
-    for (int i=0; i < word.length(); i++) {
-        s += time[word[i] - 65];
+    string name;
+    while (getline(cin, name)) {
+        cout << name << endl;
     }
-    cout << s;
 }
-
