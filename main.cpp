5e6805376a338b87e61e87ea49d0ad477b9a391a
j.hwp <we1905@icloud.com>
2024-01-26 15:27:15 +0900

    25206

--- main.cpp
+++ main.cpp
@@ -2,38 +2,77 @@
 #include <string>
 using namespace std;
 
-bool is_conti(string& s);
+class Grade {
+private:
+    string subject;
+    double score;
+    string grade;
+
+public:
+    Grade() :subject(""), score(0), grade("") {}
+    Grade(string& sub, double s, string& g);
+    double getscore();
+    double getgrade_d();
+    double total_score();
+    string getgrade();
+    double change_g_d(string);
+};
+
+Grade::Grade(string& sub, double s, string& g) :
+        subject{sub}, score(s), grade(g) {}
+
+double Grade::getscore() {
+    return score;
+}
+double Grade::getgrade_d() {
+    return change_g_d(grade);
+}
+double Grade::total_score() {
+    return change_g_d(grade) * score;
+}
+string Grade::getgrade() {
+    return grade;
+}
+double Grade::change_g_d(string g) {
+    if (g == "A+")
+        return 4.5;
+    if (g == "A0")
+        return 4.0;
+    if (g == "B+")
+        return 3.5;
+    if (g == "B0")
+        return 3.0;
+    if (g == "C+")
+        return 2.5;
+    if (g == "C0")
+        return 2.0;
+    if (g == "D+")
+        return 1.5;
+    if (g == "D0")
+        return 1.0;
+    else
+        return 0.0;
+}
 
 int main() {
-    int n;
-    string str;
-    int count = 0;
+    string subject;
+    double score;
+    string grade;
+    double total_score = 0;
+    double total_sum = 0;
+    Grade arr[20];
 
-    cin >> n;
-    for (int i=0; i<n;i++) {
-        cin >> str;
-        if (is_conti(str))
-            count++;
-    }
-    cout << count;
-}
-
-bool is_conti(string& str) {
-    bool is = true;
-
-    for (int i=0; i<str.length(); i++) {
-        int idx = str.find(str[i],i+1);
-        if (idx == -1)
-            continue;
-        else {
-            if (idx != i+1)
-                is = false;
+    for (int i = 0; i < 20; i++) {
+        cin >> subject >> score >> grade;
+        arr[i] = Grade(subject, score, grade);
+        if (arr[i].getgrade() != "P") {
+            total_score += arr[i].getscore();
+            total_sum += arr[i].total_score();
         }
     }
-    return is;
+    cout << total_sum / total_score;
+
 }
-
-
 
 
 
