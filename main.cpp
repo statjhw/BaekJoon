cf40f44c53fcd1f69d81ef07e9b001b1bdc95797
j.hwp <we1905@icloud.com>
2024-01-31 15:17:50 +0900

    11653

--- main.cpp
+++ main.cpp
@@ -1,43 +1,46 @@
 #include <iostream>
 using namespace std;
 
-int is_prime(int);
-int main()
-{
-    int n;
-    cin >> n;
-    int count = 0;
-    for (int i=0;i<n;i++)
-    {
-        int a;
-        cin >> a;
-        if (is_prime(a)) count++;
-    }
-    cout << count;
-}
-
-int is_prime(int n)
-{
-    bool is = true;
-    if (n == 1)
-        is = false;
-
-    if (n > 2)
-    {
-        for (int i = 2; i < n; i++)
-        {
-            if (n % i == 0)
-            {
-                is = false;
-                break;
-            }
-        }
-    }
-    return is;
-}
-
-
-
+//int is_prime(int);
+//
+//int main()
+//{
+//    int n;
+//    cin >> n;
+//    if (n == 1){}
+//    else {
+//        while (!is_prime(n)) {
+//            for (int i = 2; i < n; i++) {
+//                if (n % i == 0) {
+//                    cout << i << endl;
+//                    n /= i;
+//                    break;
+//                }
+//            }
+//        }
+//        cout << n;
+//    }
+//}
+//
+//int is_prime(int n)
+//{
+//    bool is = true;
+//    if (n == 1)
+//        is = false;
+//
+//    if (n > 2)
+//    {
+//        for (int i = 2; i * i <= n; i++)
+//        {
+//            if (n % i == 0)
+//            {
+//                is = false;
+//                break;
+//            }
+//        }
+//    }
+//    return is;
+//}
 
 
 
